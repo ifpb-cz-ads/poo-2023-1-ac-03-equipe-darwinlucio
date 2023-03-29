bd9ec12f8abffa26ab305e57532351cee620cd41
João Darwin <107586789+Joao-Darwin@users.noreply.github.com>
2023-03-29 09:59:50 -0300

    Create AtividadePratica01.java

---
+++ AtividadePratica01.java
@@ -0,0 +1,13 @@
+public class AtividadePratica01 {
+	public static void main(String[] args){
+		int valor1 = 10;
+		int valor2 = 17;
+		int valor3 = 89;
+
+		int soma = valor1 + valor2 + valor3;
+		int media = soma/3;
+
+		System.out.println("Soma: " + soma);
+		System.out.println("Media: " + media);
+	}
+}
